4cf45490daf71efde212bfaf6605492177b0e379
Nish Singaraju <nishsingaraju@Nishs-MacBook-Pro.local>
2023-02-22 05:38:15 -0800

    test

--- gitlink/secret_resolvers/drain/MountDrain.py
+++ gitlink/secret_resolvers/drain/MountDrain.py
@@ -1,9 +1,9 @@
-from gitlink.secret_resolvers.drain import base_drain
+from gitlink.secret_resolvers.drain import BaseDrain
 
-class MountDrain(base_drain.SecretDrain):
+class MountDrain(BaseDrain.BaseDrain):
 
     def resolve_secret(self, secret_name):
-        secret_path = base_drain.SecretDrain._secrets_mount_dir_path() / secret_name
+        secret_path = BaseDrain.BaseDrain._secrets_mount_dir_path() / secret_name
         if secret_path.exists() and secret_path.is_file():
             with secret_path.open() as secret_file:
                 secret_value = secret_file.read()
